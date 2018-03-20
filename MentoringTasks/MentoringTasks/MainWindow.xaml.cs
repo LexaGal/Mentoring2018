7f2a71bf65ff1ab8606269f893f9f0c21c9f41e1
Aliaksei_Halushkin <Aliaksei_Halushkin@epam.com>
2018-03-20 18:31:47 +0300

    Task 1: download manager

---
+++ MentoringTasks/MentoringTasks/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Path = System.IO.Path;
+
+namespace MentoringTasks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CancellationTokenSource _tokenSource;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;             
+        }
+
+        private async void LoadPage(object sender, RoutedEventArgs e)
+        {
+            var downloadableFile = Url.Text;
+            var destinationFilePath = Path.GetFullPath($"test_{DateTime.Now.ToFileTime()}.zip");
+            _tokenSource = new CancellationTokenSource();
+            var token = _tokenSource.Token;
+
+            using (var client = new HttpClientDownloadWithProgress(downloadableFile, destinationFilePath, token))
+            {
+                client.ProgressChanged += (totalFileSize, totalBytesDownloaded, progressPercentage) =>
+                {
+                    if (progressPercentage != null)
+                    {
+                        ProgressBar.Value = progressPercentage.Value;
+                        Percentage.Text = $"{progressPercentage} %";
+                    }
+                };
+                try
+                {
+                    await client.StartDownload();
+                }
+                catch (HttpRequestException exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+                catch (TaskCanceledException exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }            
+        }
+
+        private void CancelLoad(object sender, RoutedEventArgs e)
+        {
+            _tokenSource?.Cancel();        
+        }
+    }
+}
