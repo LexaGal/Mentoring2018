6ab8bcf059b889d3cd9253a33e77dcc77a8e8ebf
Aliaksei_Halushkin <Aliaksei_Halushkin@epam.com>
2018-03-27 18:34:19 +0300

    Task 7: Rx

---
+++ MentoringTasks/MentoringTasks/RxTest.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    Rx.Observable.fromEvent(document.querySelector('#input'), 'input')
+        .debounceTime(1000)
+        .map(event => event.target.value)
+        .subscribe(value => document.querySelector('#history').appendChild(document.createElement('li')).append(value));
+});
